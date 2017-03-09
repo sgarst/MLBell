2cfaac27afb56e01aa85f05244ce379379644c5c
Sam Garst <sgarst@gmail.com>
2017-03-08 20:42:04 -0500

    Second commit...

--- MM2.py
+++ MM2.py
@@ -2,9 +2,8 @@
 # mlb_monitor.py - application to monitor a current game.
 
 import sys, getopt, time
-#import pigpio
-#from bell import bell
-#from urllib2 import Request, urlopen, URLError
+import pigpio
+from bell import bell
 import requests
 import xml.etree.cElementTree as ET
 
@@ -46,16 +45,14 @@ def main(argv):
 def parse_game(url, team): #Parse miniscoreboard.xml
     try:
         req = requests.get(url, timeout=10)
-    except req.RequestException as e:
+    except requests.exceptions.RequestException as e:
         print 'parse_game request error: ', e
     else:
-#        print 'parge_game request success: ', req.status_code
         root = ET.fromstring(req.text)
 
     #### LOCAL FILE TEST ####
     # ms = ET.parse(open("miniscoreboard.xml", "r"))
     
-#    root = ms.getroot()
     status = root.find('game_status[@status]').get('status')
     date = root.get('id')[0:9]
     runs = root.find('linescore/r')
@@ -75,8 +72,9 @@ def parse_game(url, team): #Parse miniscoreboard.xml
 def update_game(g): #update game in progress
     try:
         req = requests.get(g.gameuri, timeout=10)
-    except req.RequestException as e:
-            print 'update_game request error: ', e
+    except requests.exceptions.RequestException as e:  
+        print 'update_game requests error', e
+        sys.exit(1)
     else:
         root = ET.fromstring(req.text)
         
@@ -97,7 +95,7 @@ def get_game(team, datestr = time.strftime("year_%Y/month_%m/day_%d/") ):
     games = []
     try:
         req = requests.get(url, timeout=10)
-    except req.RequestException as e:
+    except requests.exceptions.RequestException as e:
         print 'get_game request error: ', e
     else:
         root = ET.fromstring(req.text)
@@ -126,14 +124,15 @@ if __name__ == "__main__":
             g = parse_game(url,t)
             print "Time [INNING]\t%s R (HR)\t%s R (HR)"%(g.home, g.away)
             print "%s [%s]\t    %s (%s)\t    %s (%s)"%(g.lastupdate, g.inning, g.homeR, g.homeHR, g.awayR, g.awayHR)
-            while (g.status in ["In Progress","Warming Up"]):
-                score = [g.homeR, g.homeHR, g.awayR, g.awayHR]
+            score = [g.homeR, g.homeHR, g.awayR, g.awayHR]
+            while (g.status in ["In Progress","Warmup"]):
+                print "Score: ", score[0:4]
                 g = update_game(g)
                 if not(score == [g.homeR, g.homeHR, g.awayR, g.awayHR]):
                     # IF A HOMERUN, RING BELL!!!
                     if ((not(g.homeHR == score[1]) and (t == g.home)) or (not(g.awayHR == score[3]) and (t == g.away))):
-#                        bell()
-			print "\nBELL BELL BELL\n"
+                        bell()
+                        print "\tBELL BELL BELL\n"
             	score = [g.homeR, g.homeHR, g.awayR, g.awayHR]
-                time.sleep(30)
+                time.sleep(15)
             print "Status: %s" % (g.status)
---
+++ bell.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# bell.py - Function to move bell, play audio and blink lights (threaded)
+
+import time
+from threading import Thread
+import pygame
+import pigpio
+
+def blinkLight():
+    led = pigpio.pi() # Connect to local Pi.
+    start = time.time()
+    while (time.time() - start) < 5: # Spin for 5 seconds.
+	led.write(25, 0) # set Pi's gpio 25 low
+	time.sleep(0.5)
+	led.write(25, 1) # set Pi's gpio 25 HIGH
+	time.sleep(0.5)
+
+    led.write(25, 0) # Make sure it is OFF!
+    led.stop
+
+def playAudio():
+    pygame.mixer.init()
+    pygame.mixer.music.load("/home/mlb/basebell/client/WAV/bell_1_5s.wav")
+    pygame.mixer.music.set_volume(0.9)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy() == True:
+        continue
+
+def moveBell():
+    SERVO = 17     # Servo connected to gpio 17.
+    DIR   = 1
+    PW    = 1500
+    SPEED = 50
+
+    pi = pigpio.pi() # Connect to local Pi.
+    pi.set_mode(SERVO, pigpio.OUTPUT) # Set gpio as an output.
+    start = time.time()
+    while (time.time() - start) < 5: # Spin for 5 seconds.
+        pi.set_servo_pulsewidth(SERVO, PW)
+        PW += (DIR * SPEED)
+        if (PW < 1500) or (PW > 1900): # Bounce back at safe limits.
+             DIR = - DIR
+        time.sleep(0.05)
+
+    pi.set_servo_pulsewidth(SERVO, 0) # Switch servo pulses off.
+    pi.stop()
+
+def bell():
+    a = Thread(target=playAudio)
+    a.start()
+    b = Thread(target=moveBell)
+    b.start()
+    c = Thread(target=blinkLight)
+    c.start()
+
+
+    if (not a.isAlive()):
+            a.join()
+    if (not b.isAlive()):
+            b.join()
+    if (not c.isAlive()):
+            c.join()
+

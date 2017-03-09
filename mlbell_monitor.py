b93c927133d52e047252e6595a14ac2d9dbcfbf1
Sam Garst <sgarst@gmail.com>
2017-03-09 18:00:36 -0500

    Moved MM2 insanity to mlbell_monitor.py

--- mlbell_monitor.py
+++ mlbell_monitor.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python
 # mlb_monitor.py - application to monitor a current game.
 
-import sys, getopt, time, pigpio
+import sys, getopt, time
+import pigpio
 from bell import bell
-from urllib2 import Request, urlopen, URLError
+import requests
 import xml.etree.cElementTree as ET
 
 class Game:
-    def __init__(self, gameuri, status, home, away, date):
+    def __init__(self, gameuri, status, inning, home, away, date):
         self.gameuri = gameuri
         self.status = status
+        self.inninv = inning
         self.lastupdate = time.strftime("%H:%M:%S")
         self.home = home
         self.away = away
@@ -41,31 +43,26 @@ def main(argv):
    return team, gameuri
 
 def parse_game(url, team): #Parse miniscoreboard.xml
-    req = Request(url)
     try:
-        response = urlopen(req)
-    except URLError as e:
-        if hasattr(e, 'reason'):
-            print 'We failed to reach a server.'
-            print 'Reason: ', e.reason
-        elif hasattr(e, 'code'):
-            print 'The server couldn\'t fulfill the request.'
-            print 'Error code: ', e.code
+        req = requests.get(url, timeout=10)
+    except requests.exceptions.RequestException as e:
+        print 'parse_game request error: ', e
     else:
-        ms = ET.parse(response)
+        root = ET.fromstring(req.text)
 
     #### LOCAL FILE TEST ####
     # ms = ET.parse(open("miniscoreboard.xml", "r"))
     
-    root = ms.getroot()
     status = root.find('game_status[@status]').get('status')
     date = root.get('id')[0:9]
     runs = root.find('linescore/r')
     homeruns = root.find('linescore/hr')
+    inning = root.find('game_status[@inning]').get('inning')
     
     # Create game object, return
-    g = Game(url, status, root.get('home_name_abbrev'), root.get('away_name_abbrev'), date)
+    g = Game(url, status, inning, root.get('home_name_abbrev'), root.get('away_name_abbrev'), date)
     g.lastupdate = time.strftime("%H:%M:%S")
+    g.inning = inning
     g.homeR = runs.get('home')
     g.awayR = runs.get('away')
     g.homeHR = homeruns.get('home')
@@ -73,23 +70,18 @@ def parse_game(url, team): #Parse miniscoreboard.xml
     return(g)
 
 def update_game(g): #update game in progress
-    req = Request(g.gameuri)
     try:
-        response = urlopen(req)
-    except URLError as e:
-        if hasattr(e, 'reason'):
-            print 'We failed to reach a server.'
-            print 'Reason: ', e.reason
-        elif hasattr(e, 'code'):
-            print 'The server couldn\'t fulfill the request.'
-            print 'Error code: ', e.code
+        req = requests.get(g.gameuri, timeout=10)
+    	except requests.exceptions.RequestException as e:  
+        print 'update_game requests error', e
+        sys.exit(1)
     else:
-        ms = ET.parse(response)
-
-    root = ms.getroot()
+        root = ET.fromstring(req.text)
+        
     runs = root.find('linescore/r')
     homeruns = root.find('linescore/hr')
     g.status = root.find('game_status[@status]').get('status')
+    g.inning = root.find('game_status[@inning]').get('inning')
     g.lastupdate = time.strftime("%H:%M:%S")
     g.homeR = runs.get('home')
     g.homeHR = homeruns.get('home')
@@ -100,29 +92,22 @@ def update_game(g): #update game in progress
 def get_game(team, datestr = time.strftime("year_%Y/month_%m/day_%d/") ):
 # Build a list of all games that "team" is playing "datestr" (or today, if datestr not passed in)
     url = "http://gd2.mlb.com/components/game/mlb/" + datestr + "epg.xml"
-    req = Request(url)
     games = []
     try:
-        response = urlopen(req)
-    except URLError as e:
-        if hasattr(e, 'reason'):
-            print 'We failed to reach a server.'
-            print 'Reason: ', e.reason
-        elif hasattr(e, 'code'):
-            print 'The server couldn\'t fulfill the request.'
-            print 'Error code: ', e.code
+        req = requests.get(url, timeout=10)
+    except requests.exceptions.RequestException as e:
+        print 'get_game request error: ', e
     else:
-        epg = ET.parse(response)
-
+        root = ET.fromstring(req.text)
+        
     #### LOCAL FILE TEST ####
-    #epg = ET.parse(open("epg2.xml", "r"))
+    #epg = /.parse(open("epg2.xml", "r"))
     #### END LOCAL FILE TEST ####
     
     #Now, parse epg.xml, looking for any games with "Team"
-    root = epg.getroot()
     for g in (root.findall('game/[@home_name_abbrev="{}"]'.format(team)) + root.findall('game/[@away_name_abbrev="{}"]'.format(team))):
         gdate = g.get('time_date') + ' ' + g.get('ampm') + ' (' + g.get('time_zone') + ')' 
-        game = Game(g.get('game_data_directory'), g.get('status'), g.get('home_name_abbrev'), g.get('away_name_abbrev'), gdate)
+        game = Game(g.get('game_data_directory'), g.get('status'), g.get('inning'), g.get('home_name_abbrev'), g.get('away_name_abbrev'), gdate)
         games.append(game)
     return games
 
@@ -134,19 +119,20 @@ if __name__ == "__main__":
     for game in games: 
         print '\t%s vs %s at %s (%s).'  %(game.home, game.away, game.date, game.status)
     for game in games: 
-        if (game.status in ["In Progress", "Warming Up"]):
+        if (game.status in ["In Progress","Warming Up"]):
             url = "http://gd2.mlb.com" + game.gameuri + "/miniscoreboard.xml"
             g = parse_game(url,t)
-            print "Time\t\t%s R (HR)\t%s R (HR)"%(g.home, g.away)
-            print "%s\t    %s (%s)\t    %s (%s)"%(g.lastupdate, g.homeR, g.homeHR, g.awayR, g.awayHR)
-            while (g.status == "In Progress"):
-                score = [g.homeR, g.homeHR, g.awayR, g.awayHR]
+            print "Time [INNING]\t%s R (HR)\t%s R (HR)"%(g.home, g.away)
+            print "%s [%s]\t    %s (%s)\t    %s (%s)"%(g.lastupdate, g.inning, g.homeR, g.homeHR, g.awayR, g.awayHR)
+            score = [g.homeR, g.homeHR, g.awayR, g.awayHR]
+            while (g.status in ["In Progress","Warmup"]):
+                print "Score: ", score[0:4]
                 g = update_game(g)
                 if not(score == [g.homeR, g.homeHR, g.awayR, g.awayHR]):
-                    print "%s\t    %s (%s)\t    %s (%s)"%(g.lastupdate, g.homeR, g.homeHR, g.awayR, g.awayHR)
                     # IF A HOMERUN, RING BELL!!!
-                    if ((not(g.homeHR == score.homeHR) and (t == g.home)) or (not(g.awayHR == score.awayHR) and (t == g.away))):
+                    if ((not(g.homeHR == score[1]) and (t == g.home)) or (not(g.awayHR == score[3]) and (t == g.away))):
                         bell()
-                    score = [g.homeR, g.homeHR, g.awayR, g.awayHR]
-                time.sleep(30)
+                        print "\tBELL BELL BELL\n"
+            	score = [g.homeR, g.homeHR, g.awayR, g.awayHR]
+                time.sleep(15)
             print "Status: %s" % (g.status)

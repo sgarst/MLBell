fff27d271ae8fe70cc61d527c3fadfe735bfd972
sgarst <sgarst@gmail.com>
2017-04-04 02:38:38 +0000

    status.sh cleanedup.

--- status.sh
+++ status.sh
@@ -4,8 +4,16 @@
 
 echo "Crontab:"
 crontab -l
-echo "PS:"
-ps -e | grep monitor
-echo "Flag files:"
-ls /tmp/cron*
+echo -e "\nPS:"
+if ps -e | grep -q monitor
+then ps -e | grep monitor
+else echo "No monitor running!"
+fi
+echo -e "\nFlag files:"
+if test -n "$(find /tmp -name 'cron*' -print -quit)"
+then
+    ls -l /tmp/cron*
+else
+    echo "No cron file(s) found!"
+fi
 

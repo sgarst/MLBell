f0b9e14d901e8430bcaa004652aef81d72840a97
Sam Garst <sgarst@gmail.com>
2017-04-05 14:24:01 -0400

    Updated status.sh...

--- status.sh
+++ status.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-# mlbell_status.sh - script to dump status on mlbell...
-# 3/28/2017
+# status.sh - script to dump status on mlbell...
+# SWGarst 4/5/2017
 
 echo "Crontab:"
 crontab -l
-echo "PS:"
+echo -e "\nPS:"
 ps -e | grep monitor
-echo "Flag files:"
-ls /tmp/cron*
+if test -n "$(find /tmp -name 'cron*' -print -quit)"
+then 
+   echo -e "\nCron flags:" 
+   ls -l /tmp/cron*
+else 
+   echo "No cron flags found.."
+fi
 
